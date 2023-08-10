4e6aa3ac4f86ed564fe3a5f88b285a6947ca6990
maxnourr <140822167+maxnourr@users.noreply.github.com>
2023-08-10 14:24:21 +0200

    Add files via upload

---
+++ mini_jeu_10/seringue_true.gd
@@ -0,0 +1,28 @@
+extends Area2D
+var fill = 0
+var rng = RandomNumberGenerator.new()
+
+
+func _ready():
+	var firstrandom = rng.randf_range(1, 2.9)
+	fill = floor(firstrandom) * 5
+	
+	#pour donner une valeur initiale à fill random, pas super utile tbh
+	
+	
+
+
+
+func _process(delta):
+	$Label.text = str(fill)
+	var mouse_x = get_viewport().get_mouse_position().x
+	var mouse_y = get_viewport().get_mouse_position().y
+	position.x = mouse_x
+	position.y = mouse_y
+	# scotch la seringue à la souris
+	if Input.is_action_just_pressed("click") and fill > 0:
+		fill = fill - 5
+		#vide la seringue, possible même en dehors d'une fiole
+		
+	
+
