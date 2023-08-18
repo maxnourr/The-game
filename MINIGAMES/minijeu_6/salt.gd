ecd24d9ecc6a9d866511c67e27ea3177f4d7c430
Jade Therras <jade.therras@epfl.ch>
2023-08-18 17:33:30 +0200

    add change

--- MINIGAMES/minijeu_6/salt.gd
+++ MINIGAMES/minijeu_6/salt.gd
@@ -9,6 +9,8 @@ func _process(delta):
 	if moving:
 		var mouse_x = get_viewport().get_mouse_position().x
 		var mouse_y = get_viewport().get_mouse_position().y
-		decalage += rng.randf_range(-amplitude,amplitude) 
+		decalage += rng.randf_range(-amplitude,amplitude)
+		if abs(decalage) >= 500:
+			decalage = 0
 		position.x = mouse_x + decalage
 		position.y = mouse_y
